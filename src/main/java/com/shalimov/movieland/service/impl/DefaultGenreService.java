76ca7e201df144044e01f9b9a54b9879a9462e71
DmitriyShalimov <dmitriyshalimov@gmail.com>
2018-10-17 21:16:40 +0300

    add genre cache

--- src/main/java/com/shalimov/movieland/service/impl/DefaultGenreService.java
+++ src/main/java/com/shalimov/movieland/service/impl/DefaultGenreService.java
@@ -3,14 +3,20 @@ package com.shalimov.movieland.service.impl;
 import com.shalimov.movieland.dao.GenreDao;
 import com.shalimov.movieland.entity.Genre;
 import com.shalimov.movieland.service.GenreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DefaultGenreService implements GenreService {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final GenreDao genreDao;
+    private List<Genre> allGenres;
 
     @Autowired
     public DefaultGenreService(GenreDao genreDao) {
@@ -19,6 +25,14 @@ public class DefaultGenreService implements GenreService {
 
     @Override
     public List<Genre> getAll() {
-        return genreDao.getAll();
+        return new ArrayList<>(allGenres);
     }
+
+    @Scheduled(fixedDelay = 4 * 60 * 60 * 1000, initialDelay = 0)
+    private void invalidate() {
+        logger.info("Update genre cache");
+        allGenres = genreDao.getAll();
+    }
+
+
 }
